a1f3a4d5ac466c9dfdddd620e597b498e9d5406f
Tasveer Singh <taz@tazsingh.com>
2013-10-29 17:49:06 -0400

    Don't load both test_unit and rspec integrations

--- lib/shoulda/callback/matchers.rb
+++ lib/shoulda/callback/matchers.rb
@@ -2,6 +2,6 @@ require 'shoulda/callback/matchers/version'
 
 if defined?(RSpec)
   require 'shoulda/callback/matchers/integrations/rspec'
+else
+  require 'shoulda/callback/matchers/integrations/test_unit'
 end
-
-require 'shoulda/callback/matchers/integrations/test_unit'
